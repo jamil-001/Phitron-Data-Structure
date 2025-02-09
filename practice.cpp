977d49de0ec049ab7bbed1861949ecd0bd92ed94
Jamil Ahmed <89215004+jamil-001@users.noreply.github.com>
2025-02-09 12:03:19 +0600

    Binary Tree M-17
    i.inorder
    ii.preorder
    iii.postorder

--- practice.cpp
+++ practice.cpp
@@ -1,19 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
-void print(int i,int n)
+class Node
 {
-    if(n < i){
-        return;
+public:
+    int val;
+    Node *left;
+    Node* right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
     }
-    cout << i <<endl;
-    print(i+1,n);
-    
-    
+};
+
+Node* tree_input(){
+    int val;
+    cin >> val;
+    queue<Node*>q;
+    Node* root;
+    if(val == -1) root = NULL;
+    else root = new Node(val);
+    if(root) q.push(root);
+    while(!q.empty())
+    {
+        Node* p = q.front();
+        q.pop();
+
+        int l,r;
+        cin >> l >> r;
+        Node* myleft,* myright;
+        if(l == -1) myleft = NULL;
+        else  myleft = new Node(l);
+        if(r == -1) myright = NULL;
+        else myright = new Node(r);
+
+        p->left = myleft;
+        p->right = myright;
+
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+    }
+    return root;
+}
+void postorder(Node* root)
+{
+    if(root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->val <<" ";
 }
 int main(){
-    int n;
-    cin >> n;
-    print(1,n);
-
+    Node* root = tree_input();
+    postorder(root);
     return 0;
 }
